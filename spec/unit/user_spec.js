1847f06b8463859e7031f65f8cd76d7740ad4c3b
ahallam <alexhallam75@gmail.com>
2019-01-14 12:35:08 -0500

    Finished user signup. All tests passing.

--- spec/unit/user_spec.js
+++ spec/unit/user_spec.js
@@ -1,3 +1,6 @@
+const request = require("request");
+const server = require("../../src/server");
+const base = "http://localhost:3000/users/";
 const sequelize = require("../../src/db/models/index").sequelize;
 const User = require("../../src/db/models").User;
 
