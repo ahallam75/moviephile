c4953f02ae6a981df2f273203694e0ed38a60072
ahallam <alexhallam75@gmail.com>
2019-01-21 13:04:03 -0500

    Initial movieController and routes created. Test passes.

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,8 +2,10 @@ module.exports = {
     init(app){
       const staticRoutes = require("../routes/static");
       const userRoutes = require("../routes/users");
+      const movieRoutes = require("../routes/movies");
 
       app.use(staticRoutes);
       app.use(userRoutes);
+      app.use(movieRoutes);
     }
   }
